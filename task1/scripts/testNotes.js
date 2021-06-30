3c6daf82b3f5c34b5a62d9d52f02ad5408259f19
pixel-pixel <bartishco@gmail.com>
2021-06-30 12:03:37 +0300

    add test notes

---
+++ task1/scripts/testNotes.js
@@ -0,0 +1,58 @@
+import store from "./store/store.js";
+import {createNote} from "./store/actionCreators.js";
+
+store.dispatch(createNote(
+  'Shopping list',
+  'April 20, 2021',
+  'Task',
+  'Tomatoes, bread',
+  '', false
+))
+
+store.dispatch(createNote(
+  'The theory...',
+  'April 27, 2021',
+  'Random Thought',
+  'The evolution...',
+  '', false
+))
+
+store.dispatch(createNote(
+  'New feature',
+  'May 05, 2021',
+  'Idea',
+  'Implement new...',
+  '3/5/2021 5/5/2021', false
+))
+
+store.dispatch(createNote(
+  'Books',
+  'May 15, 2021',
+  'Task',
+  'The lean startup',
+  '', false
+))
+
+store.dispatch(createNote(
+  'Shopping list2',
+  'April 20, 2022',
+  'Task',
+  'Tomatoes, bread and milk!',
+  '', false
+))
+
+store.dispatch(createNote(
+  'The theory... 2',
+  'April 27, 2022',
+  'Random Thought',
+  'The evolution of number 2',
+  '', false
+))
+
+store.dispatch(createNote(
+  'Super-New feature',
+  'May 05, 2022',
+  'Idea',
+  'Implement super-new...',
+  '3/5/2022 5/5/2022', false
+))
